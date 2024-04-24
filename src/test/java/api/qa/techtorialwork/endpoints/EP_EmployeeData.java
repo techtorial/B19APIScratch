45560d0b09ade46c4ed23e686b36b35e97982a22
davidtrn <mturanusa@gmail.com>
2024-04-24 17:51:41 -0500

    employee api testing

---
+++ src/test/java/api/qa/techtorialwork/endpoints/EP_EmployeeData.java
@@ -0,0 +1,10 @@
+package api.qa.techtorialwork.endpoints;
+
+public class EP_EmployeeData {
+
+    public static void main(String[] args) {
+
+        System.out.println("Test the new file to push to the github.");
+
+    }
+}
